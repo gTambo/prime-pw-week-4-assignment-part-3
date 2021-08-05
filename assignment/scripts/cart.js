1e58c3d68f869ad6fd71323dc43ead70c937ec44
Greg Tambornino <gregtambornino@gmail.com>
2021-08-05 00:29:08 -0500

    made 2nd and 3rd functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,19 +4,29 @@ console.log('***** Cart Functions *****');
 
 // Create a global variable named basket and set it to an empty array.
 let basket = [];
+console.log('created empty basket');
 
 //Create a function called addItem. It should:
 function addItem( string ){ //take an input parameter for a string item
+  console.log('in addItem');
   basket.push(string); //add the new item to the global array basket.
   return true; //return true indicating the item was added
 }
+
 //Create a function called listItems. It should:
-function listItems(){
-   // loop over the items in the basket array
-   // console.log each individual item on a new line
+function listItems( array ){
+  console.log('in listItems');
+  for (let i in array) { // loop over the items in the basket array
+    console.log(array[i]);
+  }  // console.log each individual item on a new line
+  return 'currently in cart';
 }
 
 // Create a function called empty. It should:
-function empty(){
-  // reset the basket to an empty array
-} 
+function empty( array ){
+  console.log('in empty');
+  for (let i = array.length-1; i >= 0; i--){
+    array.shift();
+  }
+  return array; // reset the basket to an empty array
+}
